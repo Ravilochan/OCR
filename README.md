6dd06f3d693341018214689753e701f2f62af289
Ravilochan2000 <ravilochan2000@gmail.com>
2019-10-23 07:44:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-tesseractjs-ocr
+
---
+++ server.js
@@ -0,0 +1,90 @@
+// Declaring 
+const express = require('express');
+const multer = require('multer');
+const app = express();
+const fs = require('fs');
+const pdf = require('pdfkit');
+var Tesseract = require('tesseract.js');
+
+//middlewares
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const PORT = process.env.PORT | 5000;
+
+var Storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, __dirname + '/images');
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  }
+});
+
+var upload = multer({
+  storage: Storage
+}).single('image');
+
+//route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+  console.log(req.file);
+  upload(req, res, err => {
+    if (err) {
+      console.log(err);
+      return res.send('Something went wrong');
+    }
+
+    var image = fs.readFileSync(
+      __dirname + '/images/' + req.file.originalname,
+      {
+        encoding: null
+      }
+    );
+    Tesseract.recognize(image)
+      .progress(function(p) {
+        console.log('progress', p);
+      })
+      .then(function(result) {
+       // res.send(result.text);
+        res.render('display', {
+          data: result.text , path: __dirname + '/images/' + req.file.originalname,
+      });
+        
+
+      var myDoc = new pdf;
+      myDoc.pipe(fs.createWriteStream(`./pdfs/${req.file.originalname}.pdf`));
+      myDoc.font('Times-Roman')
+           .fontSize(24)
+           .text(`${result.text}`,100,100);
+      myDoc.end();     
+      const downloadpath = __dirname +'/pdfs/'+req.file.originalname.pdf;
+      app.get("/download", (req,res)=>{
+         const file = `./pdfs/${req.file.originalname}.pdf`;
+         res.download(downloadpath);
+         res.download(file);
+       });
+
+      });
+      
+  });
+});
+
+
+
+app.get("/download", (req,res)=>{
+ // const file = `./pdfs/${req.file.originalname}.pdf`;
+  res.download(downloadpath);
+});
+
+app.get('/showdata', (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
